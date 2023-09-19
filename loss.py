b6d4bae18b8881a04f6520a16e79ce22c3f940f3
maxqiqq <116487542+maxqiqq@users.noreply.github.com>
2023-09-19 18:32:18 +0800

    Update loss.py

--- loss.py
+++ loss.py
@@ -31,19 +31,19 @@ class Vgg16(nn.Module):
         return results
 
 
-def custom_mse_loss(out, gt, mask):
-    # 均方误差，回归损失函数常用误差，预测值与目标值之间差值平方和的均值
-    diff = (out - gt) ** 2
-    penalty = torch.exp(mask)
-    return torch.mean(diff * penalty)
+# def custom_mse_loss(out, gt, mask):
+#     # 均方误差，回归损失函数常用误差，预测值与目标值之间差值平方和的均值
+#     diff = (out - gt) ** 2
+#     penalty = torch.exp(mask)
+#     return torch.mean(diff * penalty)
 
 
-def compute_ssim_loss(out, gt):
-    # 结构衡量指标SSIM (Structure Similarity Index Measure) ，可以衡量图片的失真程度，也可以衡量两张图片的相似程度。
-    # 与MSE和PSNR衡量绝对误差不同，SSIM是感知模型，即更符合人眼的直观感受。、
-    # pytorch中有现成方法可以调用
-    ssim = pytorch_ssim.ssim(out, gt)
-    return 1 - ssim
+# def compute_ssim_loss(out, gt):
+#     # 结构衡量指标SSIM (Structure Similarity Index Measure) ，可以衡量图片的失真程度，也可以衡量两张图片的相似程度。
+#     # 与MSE和PSNR衡量绝对误差不同，SSIM是感知模型，即更符合人眼的直观感受。、
+#     # pytorch中有现成方法可以调用
+#     ssim = pytorch_ssim.ssim(out, gt)
+#     return 1 - ssim
 
 
 class PerceptualLossModule:
@@ -88,18 +88,18 @@ class PerceptualLossModule:
             style_loss += self.criterion(gt, gi).item()
         return style_loss/nr_feats
 
-    def compute_perceptual_loss(self, synthetic, real):
-        # 三大loss相加
-        color_loss = self.compute_color_loss(synthetic.detach(), real)
-        style_loss = self.compute_style_loss(synthetic.detach(), real)
-        content_loss = self.compute_content_loss(synthetic.detach(), real)
-        # wandb.log({
-        #     "color_loss": color_loss,
-        #     "style_loss": style_loss,
-        #     "content_loss": content_loss
-        # })
-        perceptual_loss = color_loss + style_loss + content_loss
-        return perceptual_loss
+    # def compute_perceptual_loss(self, synthetic, real):
+    #     # 三大loss相加
+    #     color_loss = self.compute_color_loss(synthetic.detach(), real)
+    #     style_loss = self.compute_style_loss(synthetic.detach(), real)
+    #     content_loss = self.compute_content_loss(synthetic.detach(), real)
+    #     # wandb.log({
+    #     #     "color_loss": color_loss,
+    #     #     "style_loss": style_loss,
+    #     #     "content_loss": content_loss
+    #     # })
+    #     perceptual_loss = color_loss + style_loss + content_loss
+    #     return perceptual_loss
 
     def compute_perceptual_loss_v(self, synthetic, real):
         # 多了个v是啥意思？？？？？？？

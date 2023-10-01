f3e94c529bc2a065cd741cc13159ee53f35604aa
maxqiqq <116487542+maxqiqq@users.noreply.github.com>
2023-10-01 10:10:30 +0800

    Update train_translator_AB.py

--- train_translator_AB.py
+++ train_translator_AB.py
@@ -297,9 +297,15 @@ if __name__ == '__main__':
                                                                                         # lab_shrmse_epoch, lab_frmse_epoch,
                                                                                         psnr_epoch)) # lab_psnr_epoch))
                                                                                         # lab_shpsnr_epoch, lab_fpsnr_epoch))
-            rmse_epoch_avg = rmse_epoch / (len(validation_set)*16)
-            if rmse_epoch_avg < best_rmse and epoch > 0:
-                    best_rmse = rmse_epoch_avg
+            if rmse_epoch < best_rmse and epoch > 1:  # >1是因为第一个epoch模型通常不好，不要保存
+                    best_rmse = rmse_epoch
                     print("Saving checkpoint for {}".format(best_rmse))
                     torch.save(translator.cpu().state_dict(), "{}/gen_sh2f.pth".format(opt.model_dir))
                     torch.save(optimizer_G.state_dict(), "{}/optimizer_ABG.pth".format(opt.model_dir))
+            
+# torch.save(translator.cpu().state_dict(), "{}/gen_sh2f.pth".format(opt.model_dir))：
+# 将最佳模型的参数保存到文件 gen_sh2f.pth 中。这里的 translator.cpu().state_dict() 是一个字典，包含了模型的所有参数。
+# translator.cpu() 是将模型的参数从 GPU 移动到 CPU，这样可以确保无论是否有 GPU，都能加载模型。
+
+# torch.save(optimizer_G.state_dict(), "{}/optimizer_ABG.pth".format(opt.model_dir))：
+# 将优化器的状态也保存到文件 optimizer_ABG.pth 中。这样在以后加载模型时，可以恢复优化器的状态，继续训练。
